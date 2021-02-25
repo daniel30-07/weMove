80777a36281009684c96c96d209325bc43011314
daniel30-07 <danielborgesdecarvalho@gmail.com>
2021-02-25 10:09:08 -0300

    Countdown component finalized

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
-import Head from "next/head";
 import { CompletedChallenges } from "../components/CompletedChalanges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import Head from "next/head";
+
 import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | WeMove</title>
+      </Head>
       <ExperienceBar />
       <section>
         <div>
